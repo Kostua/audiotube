c8d27b2d9ab6e113a9f5937f8c7ceb687fe361fe
Kostua <kostua.p@gmail.com>
2023-03-16 13:43:59 +0100

    fix provider section

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -1,11 +1,13 @@
-provider "aws" {
-  region = "us-west-2"
-}
+
 
 terraform {
   required_version = ">= 1.3"
   required_providers {
-    aws = "~> 4.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.58.0"
+    }
+
   }
   cloud {
     organization = "cloudsbits"
@@ -14,6 +16,10 @@ terraform {
       name = "audiotube-org"
     }
   }
+}
+
+provider "aws" {
+  region = "us-west-2"
 }
 
 resource "aws_organizations_organization" "audiotube" {

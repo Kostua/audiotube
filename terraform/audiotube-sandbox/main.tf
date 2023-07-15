a9bceb08d941e4f9dc38078c938dcf6203148d1c
Kostua <kostua.p@gmail.com>
2023-07-15 09:22:18 +0200

    rollback changes

--- terraform/audiotube-sandbox/main.tf
+++ terraform/audiotube-sandbox/main.tf
@@ -16,162 +16,162 @@ terraform {
 
 
 
-locals {
-  name = "ecr-ex-${replace(basename(path.cwd), "_", "-")}"
+# locals {
+#   name = "ecr-ex-${replace(basename(path.cwd), "_", "-")}"
 
-  tags = {
-    Name = local.name
-  }
-}
+#   tags = {
+#     Name = local.name
+#   }
+# }
 
-data "aws_caller_identity" "current" {}
-data "aws_partition" "current" {}
+# data "aws_caller_identity" "current" {}
+# data "aws_partition" "current" {}
 
-################################################################################
-# ECR Repository
-################################################################################
+# ################################################################################
+# # ECR Repository
+# ################################################################################
 
-module "ecr_disabled" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+# module "ecr_disabled" {
+#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-  create = false
-}
+#   create = false
+# }
 
-module "ecr" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+# module "ecr" {
+#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-  repository_name = local.name
+#   repository_name = local.name
 
-  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-  create_lifecycle_policy           = true
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1,
-        description  = "Keep last 5 images",
-        selection = {
-          tagStatus     = "tagged",
-          tagPrefixList = ["v"],
-          countType     = "imageCountMoreThan",
-          countNumber   = 5
-        },
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
+#   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+#   create_lifecycle_policy           = true
+#   repository_lifecycle_policy = jsonencode({
+#     rules = [
+#       {
+#         rulePriority = 1,
+#         description  = "Keep last 5 images",
+#         selection = {
+#           tagStatus     = "tagged",
+#           tagPrefixList = ["v"],
+#           countType     = "imageCountMoreThan",
+#           countNumber   = 5
+#         },
+#         action = {
+#           type = "expire"
+#         }
+#       }
+#     ]
+#   })
 
-  repository_force_delete = true
+#   repository_force_delete = true
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-module "public_ecr" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+# module "public_ecr" {
+#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-  repository_name = local.name
-  repository_type = "public"
+#   repository_name = local.name
+#   repository_type = "public"
 
-  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-  repository_lifecycle_policy = jsonencode({
-    rules = [
-      {
-        rulePriority = 1,
-        description  = "Keep last 30 images",
-        selection = {
-          tagStatus     = "tagged",
-          tagPrefixList = ["v"],
-          countType     = "imageCountMoreThan",
-          countNumber   = 30
-        },
-        action = {
-          type = "expire"
-        }
-      }
-    ]
-  })
+#   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+#   repository_lifecycle_policy = jsonencode({
+#     rules = [
+#       {
+#         rulePriority = 1,
+#         description  = "Keep last 30 images",
+#         selection = {
+#           tagStatus     = "tagged",
+#           tagPrefixList = ["v"],
+#           countType     = "imageCountMoreThan",
+#           countNumber   = 30
+#         },
+#         action = {
+#           type = "expire"
+#         }
+#       }
+#     ]
+#   })
 
-  public_repository_catalog_data = {
-    description       = "Docker container for some things"
-    operating_systems = ["Linux"]
-    architectures     = ["x86"]
-  }
+#   public_repository_catalog_data = {
+#     description       = "Docker container for some things"
+#     operating_systems = ["Linux"]
+#     architectures     = ["x86"]
+#   }
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-################################################################################
-# ECR Registry
-################################################################################
+# ################################################################################
+# # ECR Registry
+# ################################################################################
 
-data "aws_iam_policy_document" "registry" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
-    }
+# data "aws_iam_policy_document" "registry" {
+#   statement {
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
+#     }
 
-    actions = [
-      "ecr:ReplicateImage",
-    ]
+#     actions = [
+#       "ecr:ReplicateImage",
+#     ]
 
-    resources = [
-      module.ecr.repository_arn,
-    ]
-  }
-}
+#     resources = [
+#       module.ecr.repository_arn,
+#     ]
+#   }
+# }
 
-module "ecr_registry" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+# module "ecr_registry" {
+#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-  create_repository = false
+#   create_repository = false
 
-  # Registry Policy
-  create_registry_policy = true
-  registry_policy        = data.aws_iam_policy_document.registry.json
+#   # Registry Policy
+#   create_registry_policy = true
+#   registry_policy        = data.aws_iam_policy_document.registry.json
 
-  # Registry Pull Through Cache Rules
-  registry_pull_through_cache_rules = {
-    pub = {
-      ecr_repository_prefix = "ecr-public"
-      upstream_registry_url = "public.ecr.aws"
-    }
-  }
+#   # Registry Pull Through Cache Rules
+#   registry_pull_through_cache_rules = {
+#     pub = {
+#       ecr_repository_prefix = "ecr-public"
+#       upstream_registry_url = "public.ecr.aws"
+#     }
+#   }
 
-  # Registry Scanning Configuration
-  manage_registry_scanning_configuration = true
-  registry_scan_type                     = "ENHANCED"
-  registry_scan_rules = [
-    {
-      scan_frequency = "SCAN_ON_PUSH"
-      filter         = "*"
-      filter_type    = "WILDCARD"
-      }, {
-      scan_frequency = "CONTINUOUS_SCAN"
-      filter         = "example"
-      filter_type    = "WILDCARD"
-    }
-  ]
+#   # Registry Scanning Configuration
+#   manage_registry_scanning_configuration = true
+#   registry_scan_type                     = "ENHANCED"
+#   registry_scan_rules = [
+#     {
+#       scan_frequency = "SCAN_ON_PUSH"
+#       filter         = "*"
+#       filter_type    = "WILDCARD"
+#       }, {
+#       scan_frequency = "CONTINUOUS_SCAN"
+#       filter         = "example"
+#       filter_type    = "WILDCARD"
+#     }
+#   ]
 
-  # Registry Replication Configuration
-  create_registry_replication_configuration = false
-  registry_replication_rules = [
-    {
-      destinations = [{
-        region      = "us-west-2"
-        registry_id = data.aws_caller_identity.current.account_id
-        }, {
-        region      = "eu-west-1"
-        registry_id = data.aws_caller_identity.current.account_id
-      }]
+#   # Registry Replication Configuration
+#   create_registry_replication_configuration = false
+#   registry_replication_rules = [
+#     {
+#       destinations = [{
+#         region      = "us-west-2"
+#         registry_id = data.aws_caller_identity.current.account_id
+#         }, {
+#         region      = "eu-west-1"
+#         registry_id = data.aws_caller_identity.current.account_id
+#       }]
 
-      repository_filters = [{
-        filter      = "prod-microservice"
-        filter_type = "PREFIX_MATCH"
-      }]
-    }
-  ]
+#       repository_filters = [{
+#         filter      = "prod-microservice"
+#         filter_type = "PREFIX_MATCH"
+#       }]
+#     }
+#   ]
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }

2975eac17c280b56275b35110cf186ad5c91f279
Kostua <kostua.p@gmail.com>
2023-07-15 11:18:13 +0200

    create private ECR

--- terraform/audiotube-sandbox/main.tf
+++ terraform/audiotube-sandbox/main.tf
@@ -16,162 +16,110 @@ terraform {
 
 
 
-# locals {
-#   name = "ecr-ex-${replace(basename(path.cwd), "_", "-")}"
+locals {
+  name = "ecr-ex-${replace(basename(path.cwd), "_", "-")}"
 
-#   tags = {
-#     Name = local.name
-#   }
-# }
+  tags = {
+    Name = local.name
+  }
+}
 
-# data "aws_caller_identity" "current" {}
-# data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_partition" "current" {}
 
-# ################################################################################
-# # ECR Repository
-# ################################################################################
+################################################################################
+# ECR Repository
+################################################################################
 
-# module "ecr_disabled" {
-#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+module "ecr_disabled" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-#   create = false
-# }
+  create = true
+}
 
-# module "ecr" {
-#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+module "ecr" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-#   repository_name = local.name
+  repository_name = local.name
 
-#   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-#   create_lifecycle_policy           = true
-#   repository_lifecycle_policy = jsonencode({
-#     rules = [
-#       {
-#         rulePriority = 1,
-#         description  = "Keep last 5 images",
-#         selection = {
-#           tagStatus     = "tagged",
-#           tagPrefixList = ["v"],
-#           countType     = "imageCountMoreThan",
-#           countNumber   = 5
-#         },
-#         action = {
-#           type = "expire"
-#         }
-#       }
-#     ]
-#   })
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  create_lifecycle_policy           = true
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 5 images",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 5
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 
-#   repository_force_delete = true
+  repository_force_delete = true
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# module "public_ecr" {
-#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
+################################################################################
+# ECR Registry
+################################################################################
 
-#   repository_name = local.name
-#   repository_type = "public"
+data "aws_iam_policy_document" "registry" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
 
-#   repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
-#   repository_lifecycle_policy = jsonencode({
-#     rules = [
-#       {
-#         rulePriority = 1,
-#         description  = "Keep last 30 images",
-#         selection = {
-#           tagStatus     = "tagged",
-#           tagPrefixList = ["v"],
-#           countType     = "imageCountMoreThan",
-#           countNumber   = 30
-#         },
-#         action = {
-#           type = "expire"
-#         }
-#       }
-#     ]
-#   })
+    actions = [
+      "ecr:ReplicateImage",
+    ]
 
-#   public_repository_catalog_data = {
-#     description       = "Docker container for some things"
-#     operating_systems = ["Linux"]
-#     architectures     = ["x86"]
-#   }
+    resources = [
+      module.ecr.repository_arn,
+    ]
+  }
+}
 
-#   tags = local.tags
-# }
+module "ecr_registry" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-# ################################################################################
-# # ECR Registry
-# ################################################################################
+  create_repository = true
 
-# data "aws_iam_policy_document" "registry" {
-#   statement {
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
-#     }
+  # Registry Policy
+  create_registry_policy = true
+  registry_policy        = data.aws_iam_policy_document.registry.json
 
-#     actions = [
-#       "ecr:ReplicateImage",
-#     ]
+  # Registry Pull Through Cache Rules
+  registry_pull_through_cache_rules = {
+    pub = {
+      ecr_repository_prefix = "ecr-public"
+      upstream_registry_url = "public.ecr.aws"
+    }
+  }
 
-#     resources = [
-#       module.ecr.repository_arn,
-#     ]
-#   }
-# }
+  # Registry Scanning Configuration
+  manage_registry_scanning_configuration = true
+  registry_scan_type                     = "ENHANCED"
+  registry_scan_rules = [
+    {
+      scan_frequency = "SCAN_ON_PUSH"
+      filter         = "*"
+      filter_type    = "WILDCARD"
+      }, {
+      scan_frequency = "CONTINUOUS_SCAN"
+      filter         = "*"
+      filter_type    = "WILDCARD"
+    }
+  ]
 
-# module "ecr_registry" {
-#   source = "github.com/terraform-aws-modules/terraform-aws-ecr?ref=v1.6.0"
 
-#   create_repository = false
-
-#   # Registry Policy
-#   create_registry_policy = true
-#   registry_policy        = data.aws_iam_policy_document.registry.json
-
-#   # Registry Pull Through Cache Rules
-#   registry_pull_through_cache_rules = {
-#     pub = {
-#       ecr_repository_prefix = "ecr-public"
-#       upstream_registry_url = "public.ecr.aws"
-#     }
-#   }
-
-#   # Registry Scanning Configuration
-#   manage_registry_scanning_configuration = true
-#   registry_scan_type                     = "ENHANCED"
-#   registry_scan_rules = [
-#     {
-#       scan_frequency = "SCAN_ON_PUSH"
-#       filter         = "*"
-#       filter_type    = "WILDCARD"
-#       }, {
-#       scan_frequency = "CONTINUOUS_SCAN"
-#       filter         = "example"
-#       filter_type    = "WILDCARD"
-#     }
-#   ]
-
-#   # Registry Replication Configuration
-#   create_registry_replication_configuration = false
-#   registry_replication_rules = [
-#     {
-#       destinations = [{
-#         region      = "us-west-2"
-#         registry_id = data.aws_caller_identity.current.account_id
-#         }, {
-#         region      = "eu-west-1"
-#         registry_id = data.aws_caller_identity.current.account_id
-#       }]
-
-#       repository_filters = [{
-#         filter      = "prod-microservice"
-#         filter_type = "PREFIX_MATCH"
-#       }]
-#     }
-#   ]
-
-#   tags = local.tags
-# }
+  tags = local.tags
+}

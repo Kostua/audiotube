4c0e5a0fc4a532103f882537c16f2537e7d238e7
Kostua <kostua.p@gmail.com>
2023-07-23 15:19:45 +0200

    run only in allowed regions

--- terraform/org/variables.tf
+++ terraform/org/variables.tf
@@ -16,3 +16,9 @@ variable "account_emails" {
     audiotube-infrastructure-preprod = "audiotube-infrastructure-preprod@cloudsbits.com",
   }
 }
+
+variable "allowed_regions" {
+  type        = list(string)
+  description = "List of allowed regions"
+  default     = ["us-east-1", "us-east-2"]
+}
